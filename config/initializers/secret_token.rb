f24ac0cd68e6af63bad92f49fcd9095f8df97d91
Carlos Salgado <casalgado86@yahoo.com>
2014-09-13 10:00:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hazbun::Application.config.secret_key_base = 'b2a30ff5028b0e5a7ad234d84462af8e57378a047df12ab29f278239e143681cfe5660e2ef8a5b9fee74048ebc9ffff05f382b212414c8bfc3c11b7f3624a388'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hazbun::Application.config.session_store :cookie_store, key: '_Hazbun_session'
