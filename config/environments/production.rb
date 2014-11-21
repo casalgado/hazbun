d51897c47df904a8ebc4a584a9371f875c7198f5
Carlos Salgado <casalgado86@yahoo.com>
2014-11-21 09:29:29 -0500

    deploy - fixing faker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Hazbun::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Agregado por mi
+  config.action_mailer.default_url_options = {:host => 'informedigital.herokuapp.com'}
 end
