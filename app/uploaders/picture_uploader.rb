f7224a75a15979c5fce2efc4a49e0498cd3ab41c
Carlos Salgado <casalgado86@yahoo.com>
2014-11-22 15:17:41 -0500

    bootstrap - set customer picture size

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -32,7 +32,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
   # Create different versions of your uploaded files:
 version :thumb do
-  process :resize_to_fit => [200, 200]
+  process :resize_to_fit => [200, 140]
 end
 
   # Add a white list of extensions which are allowed to be uploaded.
