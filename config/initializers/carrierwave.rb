5b4c38c382c25f782826af5e2ddf2616401bf65d
Carlos Salgado <casalgado86@yahoo.com>
2014-12-04 12:08:56 -0500

    adding s3 - adding endpoint

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET"]                         # required
+    :aws_secret_access_key  => ENV["AWS_SECRET"],
+    :endpoint               => 'https://hazbun.app-bucket.s3-website-sa-east-1.amazonaws.com'                         # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
