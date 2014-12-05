38c876d6495e09b030e780ce2d573a47d8dbeab7
Carlos Salgado <casalgado86@yahoo.com>
2014-12-05 09:53:11 -0500

    still working on s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                                # required
     :aws_access_key_id      => ENV["AWS_ACCESS"],                    # required
     :aws_secret_access_key  => ENV["AWS_SECRET"],
-    :region                 => 'sa-east-1', 
-    :endpoint               => 'https://hazbun-app-bucket.s3-website-us-west-2.amazonaws.com'                         # required
+    :region                 => "us-west-2",
+    :endpoint               => 'https://hazbun-bucket.s3-website-us-east-1.amazonaws.com'                         # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                          # required
 end
