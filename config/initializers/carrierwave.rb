3d57a435c5e275712bd7d6f33004bbe7f5502e93
Carlos Salgado <casalgado86@yahoo.com>
2014-12-05 11:12:36 -0500

    still working on s3 - 5

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,14 @@
 
 
-CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV["AWS_BUCKET"] 
-  config.aws_acl    = :public_read
-  config.asset_host = 'http://hazbun.herokuapp.com'
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
-  config.aws_credentials = {
-    access_key_id:     ENV["AWS_ACCESS"],
-    secret_access_key: ENV["AWS_SECRET"]
+
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET"],                        # required
   }
+  config.fog_directory  = ENV["AWS_BUCKET"]                          # required
 end
 
